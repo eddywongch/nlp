365565ded7d356c537af381e081c300df907bc71
Eddy Wong <eddy.wong@datastax.com>
2020-10-14 16:49:41 -0400

    Clean up: Skipping header

--- quoteloader.py
+++ quoteloader.py
@@ -11,16 +11,22 @@ import csv
 from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize, word_tokenize 
 
+# Stop list comming from nltk
 stop_words = set(stopwords.words('english'))
 
+# My own custom stoplist
 custom_stop_words = {'.', '-', "'s", 'it','and','is','...','so'}
 
 filtered_sentence = []
+stopped_sentence = []
 
 with open('golden-quotes1.csv') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',', quotechar='\"')
-    for row in spamreader:
+    csvreader = csv.reader(csvfile, delimiter=',', quotechar='\"')
+    next(csvreader)
 
+    for row in csvreader:
+
+        # The third column is the quote
         quote = row[3]
         print("=== Quote ===")
         print(quote)
@@ -29,18 +35,21 @@ with open('golden-quotes1.csv') as csvfile:
         
         #filtered_sentence = [w for w in word_tokens if not w in stop_words]
         #filtered_sentence = [w for w in word_tokens if not w in custom_stop_words] 
+        
+        # convert to lowercase
+        filtered_sentence  = [w.lower() for w in word_tokens]
+
+        for w in filtered_sentence: 
+            if w not in stop_words and w.isalpha() and w not in custom_stop_words: 
+                stopped_sentence.append(w.lower()) 
           
-        for w in word_tokens: 
-            if w not in stop_words and w.isalpha(): 
-                filtered_sentence.append(w.lower()) 
-          
-        print("== Tokens ==")
+        print("== Raw Tokens ==")
         print(word_tokens)
 
         #filtered_sentence = [w for w in word_tokens if not w in custom_stop_words] 
 
         print("== Filtered ==") 
-        print(filtered_sentence) 
+        print(stopped_sentence) 
 
 #print("== Stop_words ==")
 #print(stop_words)
