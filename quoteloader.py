99798f0bb0815f1f2885c180d9cb095acd3ddd42
Eddy Wong <eddy.wong@datastax.com>
2020-10-20 15:35:19 -0400

    Modularizing quoloader.py: added main and functions

--- quoteloader.py
+++ quoteloader.py
@@ -1,3 +1,4 @@
+#!/bin/python3
 
 # quoteloader.py
 #
@@ -7,50 +8,109 @@
 # import the existing word and sentence tokenizing  
 # libraries 
 
+# Imports
 import csv
+# NLP imports
 from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize, word_tokenize 
+# DSE imports
+from cassandra.cluster import Cluster, EXEC_PROFILE_GRAPH_DEFAULT
+from cassandra.datastax.graph import GraphProtocol
+from cassandra.datastax.graph.fluent import DseGraph
 
-# Stop list comming from nltk
-stop_words = set(stopwords.words('english'))
 
-# My own custom stoplist
-custom_stop_words = {'.', '-', "'s", 'it','and','is','...','so'}
 
-filtered_sentence = []
-stopped_sentence = []
+class QuoteLoader:
+    def __init__(self, name):
+        self.name = name
 
-with open('golden-quotes1.csv') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',', quotechar='\"')
-    next(csvreader)
+    ############################################################
+    # def initialize(self)
+    # Class function Initialize. Any boot-up calls here, like 
+    # loading the timezone table
+    # Param: None
+    # Return: [side effect] It loads the timezone table
+    # and sets a class variable that points to it
+    ############################################################
+    def initialize(self):
+        print("Initialize")
 
-    for row in csvreader:
+    ############################################################
+    # def connect(self,ip):
+    # Class function to connect to DSE
+    # Param: ip - IP address where DSE server lives
+    # Return: [side effect] It inits a session variable that keeps
+    # a connection to DSE
+    ############################################################
+    def connect(self,ip):
+        # Not blank
+        if ip and ip.strip():
+            self.ip = ip
+        else:
+            self.ip = '10.101.33.239'
 
-        # The third column is the quote
-        quote = row[3]
-        print("=== Quote ===")
-        print(quote)
+        # Create an execution profile, using GraphSON3 for Core graphs
+        self.ep = DseGraph.create_execution_profile(
+            'gquotes',
+            graph_protocol=GraphProtocol.GRAPHSON_3_0)
+        self.cluster = Cluster(execution_profiles={EXEC_PROFILE_GRAPH_DEFAULT: self.ep}, contact_points=[self.ip])
+        self.session = self.cluster.connect()
 
-        word_tokens = word_tokenize(quote)
-        
-        #filtered_sentence = [w for w in word_tokens if not w in stop_words]
-        #filtered_sentence = [w for w in word_tokens if not w in custom_stop_words] 
-        
-        # convert to lowercase
-        filtered_sentence  = [w.lower() for w in word_tokens]
 
-        for w in filtered_sentence: 
-            if w not in stop_words and w.isalpha() and w not in custom_stop_words: 
-                stopped_sentence.append(w.lower()) 
-          
-        print("== Raw Tokens ==")
-        print(word_tokens)
+    def readFile(self, file):
+        # Stop list comming from nltk
+        stop_words = set(stopwords.words('english'))
 
-        #filtered_sentence = [w for w in word_tokens if not w in custom_stop_words] 
+        # My own custom stoplist
+        custom_stop_words = {'.', '-', "'s", 'it','and','is','...','so'}
 
-        print("== Filtered ==") 
-        print(stopped_sentence) 
+        filtered_sentence = []
+        stopped_sentence = []
 
-#print("== Stop_words ==")
-#print(stop_words)
+        with open(file) as csvfile:
+            csvreader = csv.reader(csvfile, delimiter=',', quotechar='\"')
+            
+            # skipping header
+            next(csvreader)
+
+            for row in csvreader:
+
+                # The third column is the quote
+                quote = row[5]
+                print("=== Quote ===")
+                print(quote)
+
+                word_tokens = word_tokenize(quote)
+                
+                #filtered_sentence = [w for w in word_tokens if not w in stop_words]
+                #filtered_sentence = [w for w in word_tokens if not w in custom_stop_words] 
+                
+                # convert to lowercase
+                filtered_sentence  = [w.lower() for w in word_tokens]
+
+                for w in filtered_sentence: 
+                    if w not in stop_words and w.isalpha() and w not in custom_stop_words: 
+                        stopped_sentence.append(w.lower()) 
+                  
+                print("== Raw Tokens ==")
+                print(word_tokens)
+
+                #filtered_sentence = [w for w in word_tokens if not w in custom_stop_words] 
+
+                print("== Filtered ==") 
+                print(stopped_sentence) 
+
+        #print("== Stop_words ==")
+        #print(stop_words)
+
+
+if __name__ == '__main__':
+    ql = QuoteLoader("fl1")         # Constructor
+    ql.initialize()                 # Init
+    ql.connect('10.101.33.239')     # Connect
+
+
+    ql.readFile("golden-quotes1.csv")
+    #ql.readFile("golden-quotes.csv.csv")  
+
 
